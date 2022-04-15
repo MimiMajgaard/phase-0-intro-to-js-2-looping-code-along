02c1ecac77c8bb417ab9e77a601ab9e4674201fe
Mimi Majgaard <mimimajgaard@gmail.com>
2022-04-14 18:06:40 -0700

    commit tree 395686370580c1deac533ab19e238cad0fd4baf1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const customMessage = [];
+    for (let i = 0; i < names.length; i++) {
+        const thankYouMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        customMessage.push(thankYouMessage);
+    }
+    return customMessage;
+}
+
+
+function countDown(startNum){
+    var i = startNum;
+    while(i >= 0){
+        console.log(i);
+        i--;
+    }
+}
